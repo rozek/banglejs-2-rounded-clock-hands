1887fd688ac1d3c19779f5f9bfa82f2984732e1e
Andreas Rozek <A.Rozek@gmx.de>
2022-01-10 11:44:27 +0100

    Update ClockHands.js

--- ClockHands.js
+++ ClockHands.js
@@ -1,35 +1,35 @@
 (function () {
-  let HourHandWidth   = 2*3, halfHourHandWidth   = HourHandWidth/2;
-  let MinuteHandWidth = 2*2, halfMinuteHandWidth = MinuteHandWidth/2;
+  var HourHandWidth   = 2*3, halfHourHandWidth   = HourHandWidth/2;
+  var MinuteHandWidth = 2*2, halfMinuteHandWidth = MinuteHandWidth/2;
 
-  let SecondHandOffset = halfHourHandWidth + 10;
+  var SecondHandOffset = halfHourHandWidth + 10;
 
-  let outerBoltRadius = halfHourHandWidth + 2, innerBoltRadius = outerBoltRadius - 4;
-  let HandOffset = outerBoltRadius + 4;
+  var outerBoltRadius = halfHourHandWidth + 2, innerBoltRadius = outerBoltRadius - 4;
+  var HandOffset = outerBoltRadius + 4;
 
-  let twoPi  = 2*Math.PI, deg2rad = Math.PI/180;
-  let Pi     = Math.PI;
-  let halfPi = Math.PI/2;
+  var twoPi  = 2*Math.PI, deg2rad = Math.PI/180;
+  var Pi     = Math.PI;
+  var halfPi = Math.PI/2;
 
-  let sin = Math.sin, cos = Math.cos;
+  var sin = Math.sin, cos = Math.cos;
 
-  let sine = [0, sin(30*deg2rad), sin(60*deg2rad), 1];
+  var sine = [0, sin(30*deg2rad), sin(60*deg2rad), 1];
 
-  let HandPolygon = [
+  var HandPolygon = [
     -sine[3],-sine[0], -sine[2],-sine[1], -sine[1],-sine[2], -sine[0],-sine[3],
      sine[0],-sine[3],  sine[1],-sine[2],  sine[2],-sine[1],  sine[3],-sine[0],
      sine[3], sine[0],  sine[2], sine[1],  sine[1], sine[2],  sine[0], sine[3],
     -sine[0], sine[3], -sine[1], sine[2], -sine[2], sine[1], -sine[3], sine[0],
   ];
 
-  let HourHandLength  = 0;
-  let HourHandPolygon = new Array(HandPolygon.length);
+  var HourHandLength  = 0;
+  var HourHandPolygon = new Array(HandPolygon.length);
 
   function prepareHourHandPolygon(newHourHandLength) {
     if (HourHandLength === newHourHandLength) { return; }
 
     HourHandLength = newHourHandLength;
-    for (let i = 0, l = HandPolygon.length; i < l; i+=2) {
+    for (var i = 0, l = HandPolygon.length; i < l; i+=2) {
       HourHandPolygon[i]   = halfHourHandWidth*HandPolygon[i];
       HourHandPolygon[i+1] = halfHourHandWidth*HandPolygon[i+1];
       if (i < l/2) { HourHandPolygon[i+1] -= HourHandLength; }
@@ -37,14 +37,14 @@
     }
   }
 
-  let MinuteHandLength  = 0;
-  let MinuteHandPolygon = new Array(HandPolygon.length);
+  var MinuteHandLength  = 0;
+  var MinuteHandPolygon = new Array(HandPolygon.length);
 
   function prepareMinuteHandPolygon(newMinuteHandLength) {
     if (MinuteHandLength === newMinuteHandLength) { return; }
 
     MinuteHandLength = newMinuteHandLength;
-    for (let i = 0, l = HandPolygon.length; i < l; i+=2) {
+    for (var i = 0, l = HandPolygon.length; i < l; i+=2) {
       MinuteHandPolygon[i]   = halfMinuteHandWidth*HandPolygon[i];
       MinuteHandPolygon[i+1] = halfMinuteHandWidth*HandPolygon[i+1];
       if (i < l/2) { MinuteHandPolygon[i+1] -= MinuteHandLength; }
@@ -52,12 +52,12 @@
     }
   }
 
-  let transformedPolygon = new Array(HandPolygon.length);
+  var transformedPolygon = new Array(HandPolygon.length);
 
   function transformPolygon (originalPolygon, OriginX,OriginY, Phi) {
-    let sPhi = sin(Phi), cPhi = cos(Phi), x,y;
+    var sPhi = sin(Phi), cPhi = cos(Phi), x,y;
 
-    for (let i = 0, l = originalPolygon.length; i < l; i+=2) {
+    for (var i = 0, l = originalPolygon.length; i < l; i+=2) {
       x = originalPolygon[i];
       y = originalPolygon[i+1];
 
@@ -72,8 +72,8 @@
     prepareHourHandPolygon  (outerRadius * 0.5);
     prepareMinuteHandPolygon(outerRadius * 0.8);
 
-    let HoursAngle   = (Hours+(Minutes/60))/12 * twoPi - Pi;
-    let MinutesAngle = (Minutes/60)            * twoPi - Pi;
+    var HoursAngle   = (Hours+(Minutes/60))/12 * twoPi - Pi;
+    var MinutesAngle = (Minutes/60)            * twoPi - Pi;
 
     g.setColor(Settings.Foreground === 'Theme' ? g.theme.fg : Settings.Foreground || '#000000');
 
@@ -86,11 +86,11 @@
     if (Seconds != null) {
       g.setColor(Settings.Seconds === 'Theme' ? g.theme.fgH : Settings.Seconds || '#FFFF00');
 
-      let SecondsAngle = (Seconds/60) * twoPi - Pi;
+      var SecondsAngle = (Seconds/60) * twoPi - Pi;
 
-      let sPhi = Math.sin(SecondsAngle), cPhi = Math.cos(SecondsAngle);
+      var sPhi = Math.sin(SecondsAngle), cPhi = Math.cos(SecondsAngle);
 
-      let SecondHandLength = outerRadius * 0.9;
+      var SecondHandLength = outerRadius * 0.9;
       g.drawLine(
         CenterX + SecondHandOffset*sPhi,
         CenterY - SecondHandOffset*cPhi,
